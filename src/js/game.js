5de28c8ee22a1c34ce865e9d411614cb9dea7cc4
AlicjaLysien <alicja.lysien@gmail.com>
2020-12-02 10:47:12 +0100

    game - answer buttons

--- src/js/game.js
+++ src/js/game.js
@@ -1,10 +1,23 @@
 export default (imgSrc, name1, name2, name3, name4) => {
 
+    var nameArray = [name1, name2, name3, name4]
+
+    function shuffle(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    shuffle(nameArray);
+
     let template = `
 
 
-
-            <div class="country-flag-container" id="country-flag-container">
+<div>
+            <div class="country-flag-container">
             <img id="gamePhoto"
                 src=${imgSrc}
                 alt="country flag"
@@ -12,11 +25,19 @@ export default (imgSrc, name1, name2, name3, name4) => {
             />
             </div>
             
-      ${name1}
-      ${name2}
-      ${name3}
-      ${name4}
-          
+    <div class="answerColumn">  
+    <div class="gameAnswer">${nameArray[0]}</div>
+    <div class="gameAnswer">${nameArray[1]}</div>
+    </div>
+    
+    <div class="answerColumn">  
+    <div class="gameAnswer">${nameArray[2]}</div>
+    <div class="gameAnswer">${nameArray[3]}</div>
+    </div>
+    
+    <div id="nextButton">Next</div>
+
+          </div>
 
     `
     return template;
